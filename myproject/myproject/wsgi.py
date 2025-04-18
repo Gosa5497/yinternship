dec26f8b217bb9179c81f4e68b7c9ff6f8cecefe
GOSAYE  WOYO <gosayewoyo5@gmail.com>
2025-04-18 03:07:53 +0300

    Update wsgi.py

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -2,5 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Modified for nested project structure
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 application = get_wsgi_application()
