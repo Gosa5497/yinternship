449b2a1508c17cad4b7cc052a1a66f2b28ec123a
Gosaye woyo <gosayewoyo5@gmail.com>
2025-04-19 11:50:04 +0300

    Fix wsgi.py merge conflict and update settings path

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -2,10 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Modified for nested project structure
-<<<<<<< HEAD
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.myproject.settings')
-application = get_wsgi_application()
-=======
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 application = get_wsgi_application()
->>>>>>> 13a28653f9a0efef4af998ecd9891eb247804bbd
